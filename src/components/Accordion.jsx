2cf437b2f37fee72b1d1a642c6763eeb02ee0a05
Richo Febrian <richofal25@gmail.com>
2025-01-02 18:11:46 +0700

    Modifying tampilan

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -2,7 +2,7 @@ import AccordionItem from "./AccordionItem";
 
 function Accordion({ data }) {
   return (
-    <div className="w-[700px] my-[100px] mx-auto flex flex-col gap-[24px]">
+    <div className="lg:w-[700px] my-[100px] mx-auto flex flex-col gap-[24px] w-[350px]">
       {data.map((faq, index) => (
         <AccordionItem
           key={index}
--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,9 +1,17 @@
 export default function AccordionItem({ number, question, answer }) {
   return (
-    <div className="py-[20px] px-[24px] pr-[48px] cursor-pointer border-t-[4px] border-solid border-[#fff] border-b-[4px] grid items-center accordion-item">
-      <p className="text-[24px] font-medium text-[#ced4da]">{number}</p>
-      <p className="text-[24px] font-medium">{question}</p>
-      <p className="text-[24px] font-medium">+</p>
+    <div
+      className="py-[20px] px-[24px] pr-[48px] cursor-pointer border-t-[4px] border-t-solid border-t-[#fff] 
+    border-b-[4px] border-b-solid border-b-[#fff] grid grid-cols-[auto_1fr_auto] items-center gap-x-[24px] 
+    gap-y-[32px] shadow-[0_0_30px_rgba(0,0,0,0.1)]"
+    >
+      <p className="lg:text-[24px] text-[18px] font-medium text-[#ced4da]">
+        {number}
+      </p>
+      <p className="lg:text-[24px] text-[18px] font-medium leading-[1.5]">
+        {question}
+      </p>
+      <p className="lg:text-[24px] text-[18px] font-medium">+</p>
       <div className="pb-[16px] leading-[1.6] answer">{answer}</div>
     </div>
   );
